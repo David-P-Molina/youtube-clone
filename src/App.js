72428726ea1fce4f95b557a2436515b0d60d3473
David Moina <davemolina777@gmail.com>
2023-04-20 11:32:18 -0500

    Create central import for components, Return Router in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
-import Navbar from './components/Navbar'
-import Feed from './components/Feed'
-import VideoDetail from './components/VideoDetail'
-import ChannelDetail from './components/ChannelDetail'
-import SearchFeed from './components/SearchFeed'
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components'
+
 
 const App = () => {
+  return (
     <Router>
         <Box sx={{ backgroundColor: '#000'}}>
             <Navbar />
@@ -19,8 +17,6 @@ const App = () => {
             </Routes>
         </Box>
     </Router>
-  return (
-    <div>App</div>
   )
 }
 
