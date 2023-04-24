ff9bcfb9bb5c1b5bf873983d6facf8a0b1f6ddd3
David Moina <davemolina777@gmail.com>
2023-04-23 21:10:11 -0500

    Create typography body tag with copyright

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -8,6 +8,13 @@ const Feed = () => {
           sx={{ height: { sx: 'auto', md: '92vh'}, 
           borderRight: '1px solid #3d3d3d', 
           px: {sx: 0, md: 2} }}>
+            SideBar
+            <Typography 
+              className="copyright"
+              variant="body2"
+              sx={{ mt: 1.5, color: '#fff'}}>
+              Copyright 2023 DPM Media
+            </Typography>
       </Box>
     </Stack>
   )
