baa33118bdfde8fe7c52d90f6b17e3ee9596374a
David Moina <davemolina777@gmail.com>
2023-04-25 12:26:32 -0500

    Create selected category Box and title

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -17,6 +17,13 @@ const Feed = () => {
               Copyright 2023 DPM Media
             </Typography>
       </Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex:'2'}}>
+        <Typography 
+          variant='h4'
+          fontWeight="bold" mb={2} sx={{ color: 'white'}}>
+          New<span style={{ color: '#F31503'}}> Category Placeholder</span>
+        </Typography>
+      </Box>
     </Stack>
   )
 }
