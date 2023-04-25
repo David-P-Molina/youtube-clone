a3a09c406804b0a20e37f8e8eab548a6dc98dc05
David Moina <davemolina777@gmail.com>
2023-04-25 12:36:03 -0500

    Create Video component and add to Feed.js

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import Sidebar from './Sidebar'
+import { Sidebar, Videos } from '../components'
+
 
 const Feed = () => {
   return (
@@ -23,6 +24,7 @@ const Feed = () => {
           fontWeight="bold" mb={2} sx={{ color: 'white'}}>
           New<span style={{ color: '#F31503'}}> Category Placeholder</span>
         </Typography>
+        <Videos videos={[]}/>
       </Box>
     </Stack>
   )
