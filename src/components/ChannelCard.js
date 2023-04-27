88716839a367fdcfae799d71413ae17c4d787b2c
David Moina <davemolina777@gmail.com>
2023-04-27 11:42:45 -0500

    Create Link, Image, and styling for ChannelCard component

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -5,9 +5,24 @@ import { Link } from 'react-router-dom'
 import { demoProfilePicture } from '../utilities/constants'
 
 
-const ChannelCard = () => {
+const ChannelCard = ({ channelDetail }) => {
   return (
-    <div>ChannelCard</div>
+    <Box sx={{
+      boxShadow: 'none',
+      borderRadius: '20px'
+    }}>
+      <Link to={`/channel/${ channelDetail?.id?.channelId }`}>
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#FFF'}}>
+          <CardMedia
+            image={ channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture }
+            alt={ channelDetail?.snippet?.title }
+            sx={{ borderRadius: '50%', height: '180px', width: '180px'}}
+            >
+            
+          </CardMedia>
+        </CardContent>
+      </Link>
+    </Box>
   )
 }
 
