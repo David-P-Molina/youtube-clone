24ad0299ce49435df007e58dde2114a67aeb2df0
David Moina <davemolina777@gmail.com>
2023-04-27 11:35:21 -0500

    Import UI components to ChannelCard component

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import {Box, CardContent, CardMedia, Typography } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
+import { demoProfilePicture } from '../utilities/constants'
+
 
 const ChannelCard = () => {
   return (
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -6,7 +6,7 @@ import { demoThumbnailUrl, demoVideoTitle, demoChannelTitle, demoChannelUrl, dem
 
 const VideoCard = ({ video: {id: { videoId }, snippet }}) => {
   return (
-    <Card>
+    <Card sx={{ width: { md: '320px', xs: '100%'}, boxShadow: 'none', borderRadius: 0 }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia 
           image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} 
