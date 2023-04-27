cc5f2f57dbe82f46bc2ff5062e56e27d6165c167
David Moina <davemolina777@gmail.com>
2023-04-27 11:02:36 -0500

    Create link, image and styling for VideoCard component

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -2,11 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Typography, Card, CardContent, CardMedia } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
-import { demoThumbnailUrl, demoVideoTitle, demoChannelTitle, demoChannelUrl,demoVideoUrl } from '../utilities/constants'
+import { demoThumbnailUrl, demoVideoTitle, demoChannelTitle, demoChannelUrl, demoVideoUrl } from '../utilities/constants'
 
-const VideoCard = ({ item }) => {
+const VideoCard = ({ video: {id: { videoId }, snippet }}) => {
+  console.log(videoId, snippet)
   return (
-    <div>VideoCard</div>
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia 
+          image={snippet?.thumbnails?.high?.url} 
+          alt={snippet?.title}
+          sx={{ width: 360, height: 180}}
+          />
+      </Link>
+    </Card>
   )
 }
 
