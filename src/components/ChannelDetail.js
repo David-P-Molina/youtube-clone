a7256c535d988bc088e2cbb7b9e2b0c44cf0e6f9
David Moina <davemolina777@gmail.com>
2023-04-28 11:23:11 -0500

    Fetch Channel Data, and set to state

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { Videos, ChannelCard } from './'
+import { fetchCallAPI } from '../utilities/fetchCallAPI'
 
 const ChannelDetail = () => {
+  const [ channelDetails, setChannelDetails] = useState(null);
+  const { id } = useParams();
+  useEffect(() => {
+    fetchCallAPI(`channels?part="snippet&id=${id}`)
+    .then((data) => setChannelDetails(data))
+  }, [id])
   return (
-    <div>ChannelDetail</div>
+    <Box>
+      {id}
+      {/* <Videos />
+      <ChannelCard /> */}
+    </Box>
   )
 }
 
