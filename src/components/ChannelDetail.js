8586139c2577a26f0e00b7d44d4530b7e229cf18
David Moina <davemolina777@gmail.com>
2023-04-28 12:07:17 -0500

    Restart ChannelDetails

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,26 +1,8 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { Box } from '@mui/material'
-import { Videos, ChannelCard } from './'
-import { fetchCallAPI } from '../utilities/fetchCallAPI'
 
 const ChannelDetail = () => {
-  const [ channelDetails, setChannelDetails ] = useState(null);
-  const [ videoDetails, setVideoDetails ] = useState([]);
-  const { id } = useParams();
-  useEffect(() => {
-    fetchCallAPI(`channels?part="snippet&id=${id}`)
-    .then((data) => setChannelDetails(data))
-    fetchCallAPI(`search?channelId=${id}&part=snippet&order=date`)
-    .then((data) => setVideoDetails(data))
-  }, [id])
   return (
-    <Box>
-      {id}
-      {/* <Videos />
-      <ChannelCard /> */}
-    </Box>
+    <div>ChannelDetail</div>
   )
 }
 
