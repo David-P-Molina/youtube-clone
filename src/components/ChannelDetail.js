8395882244789e2c6810104bbc3a8d39757d4549
David Moina <davemolina777@gmail.com>
2023-04-28 11:28:09 -0500

    Create fetch call for video details and assign to state

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -6,11 +6,14 @@ import { Videos, ChannelCard } from './'
 import { fetchCallAPI } from '../utilities/fetchCallAPI'
 
 const ChannelDetail = () => {
-  const [ channelDetails, setChannelDetails] = useState(null);
+  const [ channelDetails, setChannelDetails ] = useState(null);
+  const [ videoDetails, setVideoDetails ] = useState([]);
   const { id } = useParams();
   useEffect(() => {
     fetchCallAPI(`channels?part="snippet&id=${id}`)
     .then((data) => setChannelDetails(data))
+    fetchCallAPI(`search?channelId=${id}&part=snippet&order=date`)
+    .then((data) => setVideoDetails(data))
   }, [id])
   return (
     <Box>
