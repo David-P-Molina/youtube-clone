d93c09d87dadca0e435ef7dd2a7df611ad26e61f
David Moina <davemolina777@gmail.com>
2023-04-29 20:20:32 -0500

    Create initial state variables

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Videos, ChannelCard } from './'
 
 const ChannelDetail = () => {
+  const [ videos, setVideos ] = useState([])
+  const [ channelDetails, setChannelDetails ] = useState(null)
+  
   return (
     <div>ChannelDetail</div>
   )
