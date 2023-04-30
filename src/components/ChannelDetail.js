b0529b371a96d06cdeee0c9a84149d27916c0a2e
David Moina <davemolina777@gmail.com>
2023-04-29 23:55:56 -0500

    Create Component Basic layout

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
 import { Videos, ChannelCard } from './'
 import { fetchCallAPI } from '../utilities/fetchCallAPI'
 
@@ -15,7 +16,10 @@ const ChannelDetail = () => {
   
 
   return (
-    <div>ChannelDetail</div>
+    <Box>
+      <ChannelCard/>
+      <Videos />
+    </Box>
   )
 }
 
