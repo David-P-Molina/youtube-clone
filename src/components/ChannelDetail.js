6b54957c1472124c044a457503d7b38a45fb3939
David Moina <davemolina777@gmail.com>
2023-04-29 20:22:15 -0500

    Create useEffect hook with fetch calls

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Videos, ChannelCard } from './'
+import { fetchCallAPI } from '../utilities/fetchCallAPI'
 
 const ChannelDetail = () => {
   const [ videos, setVideos ] = useState([])
   const [ channelDetails, setChannelDetails ] = useState(null)
+
+  useEffect(() => {
+    const getVideos = fetchCallAPI()
+    const getChannelDetails = fetchCallAPI
+  }, [])
   
+
   return (
     <div>ChannelDetail</div>
   )
