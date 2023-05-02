a5f4fa3c06255ba853f38607025a9da60bf5bcbb
David Moina <davemolina777@gmail.com>
2023-05-02 16:10:14 -0500

    Create searchTerm params variable

--- src/components/SearchFeed.js
+++ src/components/SearchFeed.js
@@ -2,23 +2,25 @@ import { useState, useEffect } from 'react'
 import { Box, Typography } from '@mui/material'
 import { Videos } from '../components'
 import { fetchCallAPI } from '../utilities/fetchCallAPI'
+import { useParams } from 'react-router-dom'
 
 const SearchFeed = () => {
-  const [videos, setVideos] = useState([])
+  const { searchTerm } = useParams()
+  const [ videos, setVideos] = useState([])
   useEffect(() => {
-    fetchCallAPI(`search?part=snippet&q=`)
+    fetchCallAPI(`search?part=snippet&q=${searchTerm}`)
       .then((data) => {
         setVideos(data.items)
       })
       .catch((error) => console.log(error))
-  }, [])
+  }, [searchTerm])
   
   return (
     <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex:'2'}}>
       <Typography 
         variant='h4'
         fontWeight="bold" mb={2} sx={{ color: 'white'}}>
-        Search Results for:  <span style={{ color: '#F31503'}}> Videos</span>
+        Search Results for: {searchTerm} <span style={{ color: '#F31503'}}> Videos</span>
       </Typography>
       <Videos videos={videos}/>
     </Box>
