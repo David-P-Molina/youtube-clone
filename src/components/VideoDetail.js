4de555eeedd36b896e44e1a68cb9c756e53e5bc4
David Moina <davemolina777@gmail.com>
2023-05-03 15:39:38 -0500

    Destructure videoDetail information

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,21 +5,33 @@ import { Box, Stack, Typography } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material';
 
 import { fetchCallAPI } from '../utilities/fetchCallAPI';
-import { Video } from './'
 
 const VideoDetail = () => {
-  const { videoId } = useParams();
-
+  const { id } = useParams();
+  const [ videoDetail, setVideoDetail ] = useState("")
   useEffect(() => {
-    fetchCallAPI(`video?part=snippet,statistics&id=${videoId}`)
-  }, [videoId])
+    fetchCallAPI(`videos?part=snippet,statistics&id=${id}`)
+      .then((data) => setVideoDetail(data.items[0]))
+    }, [id]);
+    
+  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videoDetail
   
   return (
     <Box minHeight="95vh">
       <Stack>
         <Box flex={1}>
           <Box sx={{ width: '100%', position: 'sticky',top: '85px'}}>
-            <ReactPlayer />
+          <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
+            <Typography color="#FFF" variant="h5" fontWeight="bold" padding={2}> 
+              {title}
+            </Typography>
+            <Stack direction="row" justifyContent="space-between" sx={{ color: '#FFF' }} py={1} px={2}>
+              <Link to={`channel/${channelId}`}>
+                <Typography>
+                  {channelTitle}
+                </Typography>
+              </Link>
+            </Stack>
           </Box>
 
         </Box>
